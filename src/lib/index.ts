1f185e3cabc367fd80641edde048dde6cddf2f0b
gotomi <gotomi@gotomi.info>
2025-01-15 22:11:44 +0100

    first commit

---
+++ src/lib/index.ts
@@ -0,0 +1,2 @@
+export { default as MusicPlayer } from '$lib/MusicPlayer/MusicPlayer.svelte';
+export { default as MusicApp } from '$lib/MusicPlayer/MusicApp.svelte';
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	const music = [
+		{
+			audio:
+				'https://file-examples.com/storage/fe602ed48f677b2319947f8/2017/11/file_example_MP3_700KB.mp3',
+			title: 'tell me',
+			cover: '/covers/tell-me.jpg'
+		}
+	];
+	import { MusicApp } from '$lib/';
+</script>
+
+<MusicApp playlist={music} showPlaylist={false} />
